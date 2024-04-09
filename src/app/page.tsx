ca22d8b9ddef6c497112ea6e63530903f3f5387a
legitMinh <totallylegitperson.example@gmail.com>
2024-04-08 21:58:38 -0400

    feat: finished item component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className=" bg-white text-black h-screen">
             Hello World
             <div className=" mx-[6rem] flex overflow-scroll scroll-mx-8">
-              <ItemBox imgLink="vercel.svg" name="Snow White and the Seven Dwarfs' Cottage" rating={5} price={219.99}></ItemBox>
+              <ItemBox imgLink="/43242.webp" name="Snow White and the Seven Dwarfs' Cottage" rating={5} price={219.99}></ItemBox>
               <ItemBox imgLink="vercel.svg" name="Snow White and the Seven Dwarfs' Cottage" rating={4} price={219.99}></ItemBox>
               <ItemBox imgLink="vercel.svg" name="Snow White and the Seven Dwarfs' Cottage" rating={4.5} price={219.99}></ItemBox>
               <ItemBox imgLink="vercel.svg" name="Snow White and the Seven Dwarfs' Cottage" rating={4.25} price={219.99}></ItemBox>
--- src/components/itemBox.tsx
+++ src/components/itemBox.tsx
@@ -27,18 +27,23 @@ export default function ItemBox(props:{
     
     return (
         // Flex none to not flex(stretch)
-        <div className=" w-[24rem] h-[32rem] mx-[2rem] flex-none">
-            <div className=" w-full h-[24rem] border-neutral-200 border overflow-clip">
-                    {/* Icon on top, no collide image */}
+        <div className=" w-[24rem] h-fit mx-[2rem] flex-none">
+            <div className=" relative w-full h-[24rem] border-neutral-200 border overflow-clip">
+                {/* Icon on top, no collide image */}
                 <div className=" relative h-0"> 
-                    <div className=" absolute left-[1rem] top-[0.35rem]">
-                        ()
+                    <div className=" absolute left-[1rem] top-[0.35rem] w-[2rem] h-[2rem]">
+                        <svg width="100%" height="100%" viewBox="0 0 40 40" alt="empty" class="WishlistButtonstyles__StyledWishlistIcon-sc-1iowmvt-1 biMOpX"><rect fill="#F8F8F8" width="40" height="40" rx="20"></rect>
+                            <path d="M19.986 30l.014-.014.014.014 8.223-8.116-.018-.019a5.678 5.678 0 0 0 1.78-4.126C30 14.569 27.398 12 24.187 12A5.829 5.829 0 0 0 20 13.762 5.827 5.827 0 0 0 15.815 12C12.604 12 10 14.569 10 17.739a5.68 5.68 0 0 0 1.782 4.126" fill="#F8F8F8"></path>{/* Inner heart, make this same color with bg to make unclicked heart */}
+                            <path d="M26.84 20.417L20 27.204l-6.84-6.787A3.67 3.67 0 0 1 12 17.739C12 15.677 13.71 14 15.815 14a3.82 3.82 0 0 1 2.754 1.159l1.43 1.467 1.433-1.467A3.818 3.818 0 0 1 24.186 14C26.289 14 28 15.677 28 17.739a3.673 3.673 0 0 1-1.16 2.678M19.986 30l.014-.014.014.014 8.223-8.116-.018-.019a5.678 5.678 0 0 0 1.78-4.126C30 14.569 27.398 12 24.187 12A5.829 5.829 0 0 0 20 13.762 5.827 5.827 0 0 0 15.815 12C12.604 12 10 14.569 10 17.739a5.68 5.68 0 0 0 1.782 4.126" fill="#006DB7"></path></svg> 
                     </div>
                 </div>
                 <Image width={500} height={500} src={props.imgLink} alt="Pretty lego set image"></Image>
-                <div className=" relative"> 
-                    <div className=" absolute bottom-0">
+                {/* NEW BOX */}
+                <div className=" absolute bottom-0">
+                    <div className=" bg-yellow-400 w-[3rem] h-[1.5rem] ml-[0.5rem] mb-[0.5rem] justify-center items-center flex">
+                        <div className=" text-xs">
                         New
+                        </div>
                     </div>
                 </div>
             </div>
@@ -54,10 +59,10 @@ export default function ItemBox(props:{
                 </div>
                 
             </div>
-            <div className="h-max rounded-sm bg-orange-500">
-                <center className=" h-max">
+            <div className="h-[4rem] rounded-md bg-orange-500 justify-center items-center flex">
+                <div className=" font-semibold">
                 Add to Bag
-                </center>
+                </div>
             </div>
         </div>
     );
